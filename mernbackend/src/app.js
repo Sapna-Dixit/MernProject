b9a961295d463ceb48efbac45dc421f88a7d91d7
Sapna <sapna@webiwork.com>
2021-11-27 14:08:31 +0530

    backend task

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -4,66 +4,56 @@ require("./db/conn");
 const Register = require("./models/registers");
 const port = process.env.PORT || 8000;
 
+
+app.use(express.json());
+
 app.get("/",(req,res)=>{
     res.send("Server  is created..!!");
 });
 
+//get request for multiple documents
+app.get("/registers", async(req, res)=>{
+    try{
+         const  getEmployees = await Register.find({});
+         res.send(getEmployees);
+    }catch(e)
+    {
+        res.status(400).send(e);
+    }
+});
+
+//get request for individual documents
+
+app.get("/registers/:id", async(req, res)=>{
+    try{
+
+        const _id = req.params.id;
+        const getEmployee = await Register.findById(_id);
+        res.send(getEmployee);
+        
+
+    }catch(error)
+    {
+        res.status(400).send(error);
+    }
+})
 
 //create a new user in our database
 
-app.post("/", async(req, res)=>{
+app.post("/registers", async(req, res)=>{
     try{
-            const pass = req.body.password;
-            const cpass = req.body.confirmpassword;
-
-            if(pass === cpass)
-            {
-                const registerEmployee = new Register({
-                    firstName : req.body.firstName,
-                    lastName  : req.body.lastName,
-                       email  : req.body.email,
-                     phone    : req.body.phone,
-                     gender   : req.body.gender,
-                     age      : req.body.age,
-                     password : pass,
-              confirmpassword : cpass 
-
-                });
-            
-            const registered  = await registerEmployee.save();
-            res.status(201).render("");
-
-            }else{
-
-              res.send("password are not matching.")   
-            }
+         const addingEmployeeRecords = new Register(req.body);
+         console.log(req.body);
+        const addEmployee = await  addingEmployeeRecords.save();
+        res.status(201).send(addEmployee);
+         
     }catch(error)
     {
         res.status(400).send(error);
     }
 });
 
-//User login
 
-app.post("/", async(req, res)=>{
-    try{
-         const email = req.body.email;
-         const password = req.body.password;
-
-         const useremail = await Register.findOne({email:email});
-
-         if(useremail.pass === password)
-         {
-            res.status(201).render("");
-         }
-         else{
-            res.status(400).send("Incorrect useremail or password");
-         }
-    }catch(error)
-    {
-        res.status(400).send("Invalid login details");
-    }
-})
 
 app.listen(port, ()=>{
     console.log(`Server running at :http://localhost: ${port}`);
