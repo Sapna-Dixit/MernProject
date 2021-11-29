3cf69261092925d9e4430dfb1ba7b718fc666fdc
Sapna <sapna@webiwork.com>
2021-11-29 19:12:28 +0530

    completed task

--- mernbackend/src/server.js
+++ mernbackend/src/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const app = express();
 require("./db/conn");
-// const cors = require("cors");
-
-// app.use(express.json());
-// app.use(
-//     cors({
-//         exposedHeaders : [x-auth-token],
-//     })
-// );
-
 const port = process.env.PORT || 8000;
 const empRoute = require("./routes/empRegister");
 const loginRoute = require("./routes/empLogin");
+const cors = require("cors");
+app.use(
+    cors({
+        exposedHeaders : [x-auth-token],
+    })
+);
 
 app.use(express.json());
 app.use("/empRegister", empRoute);
